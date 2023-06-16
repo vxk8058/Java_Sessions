542fc098aa0193a6d12141ba23fefb8603b0718f
vxk8058 <kvikyatha@gmail.com>
2023-06-16 15:16:10 -0500

    hello classes

---
+++ Practice/src/com/ram/hello.java
@@ -0,0 +1,13 @@
+package com.ram;
+
+import java.util.ArrayList;
+
+public class hello {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 2; i++) {
+			System.out.println("My name is Vikyatha.");
+		}
+	}
+
+}
---
+++ Practice/src/com/ram/helloTest.java
@@ -0,0 +1,10 @@
+package com.ram;
+
+public class helloTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello world!");
+	}
+
+}
